f5e8225819c3e91244f117d39db70fef7eff21d4
QuyetTV1991 <quyettv.bk@gmail.com>
2023-11-05 22:15:38 +0800

    LeftSidebar+Bottombar+Logout+initial Root Layout

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,12 +1,22 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
-import "./globals.css"
-import { Home } from "./_root/pages"
-import SigninForm from "./_auth/forms/SigninForm"
-import SignupForm from "./_auth/forms/SignupForm"
-import AuthLayout from "./_auth/forms/AuthLayout"
-import RootLayout from "./_root/RootLayout"
-import { Toaster } from "./components/ui/toaster"
+import "./globals.css";
+import {
+  AllUsers,
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  EditPost,
+  PostDetails,
+  Profile,
+  UpdateProfile,
+} from "./_root/pages";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "./components/ui/toaster";
 
 const App = () => {
   return (
@@ -14,19 +24,27 @@ const App = () => {
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
-            <Route path="sign-in" element={<SigninForm />} />
-            <Route path="sign-up" element={<SignupForm />} />
+          <Route path="sign-in" element={<SigninForm />} />
+          <Route path="sign-up" element={<SignupForm />} />
         </Route>
 
         {/* private routes */}
         <Route element={<RootLayout />}>
-            <Route index element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
       <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ project/src/components/shared/Bottombar.tsx
@@ -0,0 +1,35 @@
+import { bottombarLinks } from "@/constant";
+import { Link, useLocation } from "react-router-dom";
+
+const Bottombar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link, i) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link
+            key={i}
+            to={link.route}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`${isActive && "invert-white"}`}
+            />
+            <p className="tinu-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Bottombar;
