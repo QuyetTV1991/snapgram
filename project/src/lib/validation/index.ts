7675d05b095b4c0e4ba24525dd83de9d162021bb
QuyetTV1991 <quyettv.bk@gmail.com>
2023-11-05 16:31:05 +0800

    initial commit + layout + signin + signup

---
+++ project/src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+import * as z from "zod";
+
+export const SignupValidationScheme = z.object({
+    name: z.string().min(2, {message: 'Too Short'}),
+    username: z.string().min(2, {message: 'Too Short'}),
+    email: z.string().email(),
+    password: z.string().min(8, {'message': "Must be at least 8 characters"})
+});
+
+export const SigninValidationScheme = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {'message': "Must be at least 8 characters"})
+});
