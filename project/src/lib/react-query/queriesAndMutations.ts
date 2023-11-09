8d9ef3df61586ddafa38e8bc9612368d70490229
QuinTran <149452198+QuinTran@users.noreply.github.com>
2023-11-09 10:51:25 +0000

    comment unuse var

--- project/src/lib/react-query/queriesAndMutations.ts
+++ project/src/lib/react-query/queriesAndMutations.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { createPost, createUserAccount, getRecentPost, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
 import { QUERY_KEYS } from './queryKeys'
