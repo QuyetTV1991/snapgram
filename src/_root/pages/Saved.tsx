1d593af34504f8047ca5fc4bbd1be00c8f02d4bf
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-03-27 09:56:47 +0000

    Saved page UI

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,5 +1,44 @@
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+
 const Saved = () => {
-  return <div>Saved</div>;
+  const { data: currentUser } = useGetCurrentUser();
+
+  const savedPosts = currentUser?.save
+    .map((savedPost: Models.Document) => ({
+      ...savedPost.post,
+      creator: {
+        imageUrl: currentUser.imageUrl,
+      },
+    }))
+    .reverse();
+
+  return (
+    <div className="saved-container">
+      <div className="flex items-center gap-2 w-full w-max-5xl">
+        <img
+          src="/assets/icons/save.svg"
+          alt="save"
+          className="invert-white w-8 h-8"
+        />
+        <h2 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h2>
+      </div>
+
+      {!currentUser ? (
+        <Loader />
+      ) : (
+        <ul>
+          {savedPosts.length === 0 ? (
+            <p>No Post Saved</p>
+          ) : (
+            <GridPostList posts={savedPosts} showStats={false} />
+          )}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Saved;
